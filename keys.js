436104543e9285066253dc7531a06cae2d82efa4
jebaquera <jebaquera@gmail.com>
2019-06-22 13:14:46 -0500

    added vars and keys

--- keys.js
+++ keys.js
@@ -1,8 +1,7 @@
-```js
+js
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.908fd0b096ed40b0bb965ac92b493d15,
-  secret: process.env.587c75689dca4c838c398c7af6e8c32d
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
-```
--- liri.js
+++ liri.js
@@ -1,16 +1,47 @@
-
-```js
+// .env file contains my api keys that are hidden via gitignore file
 require("dotenv").config();
-```
+
+
+// variables
+var keys = require("./keys.js");
+var fs = require("fs");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var request = require("request");
+var liriReturn = process.argv[2];
+var movieName = process.argv[3];
+var twitter = require("twitter");
+var client = newTwitter(keys.twitter);
+
+
+
+
+
+
+
+
+
+
 
 Add the code required to import the `keys.js` file and store it in a variable.
 
-```js
-  var keys = require("./keys.js");
-```
+js
+
   
 * You should then be able to access your keys information like so
 
-  ```js
+js
   var spotify = new Spotify(keys.spotify);
-  ```
+
+
+  // Included the axios npm package
+var axios = require("axios");
+
+// Grab the movieName which will always be the third node argument.
+var movieName = process.argv[2];
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
